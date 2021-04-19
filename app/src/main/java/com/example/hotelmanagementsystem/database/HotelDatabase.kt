a8c1a6ea0df70a1fec5fe1aca2ea8ce72893d0f6
Xinhao1221 <kohxinhao@gmail.com>
2021-04-19 21:02:14 +0800

    Changes some UI

--- app/src/main/java/com/example/hotelmanagementsystem/database/HotelDatabase.kt
+++ app/src/main/java/com/example/hotelmanagementsystem/database/HotelDatabase.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.myapp.database.HotelRoom
 
 //@Database(entities = [Reservation::class], version = 1, exportSchema = false)
 @Database(entities = [Reservation::class], version = 1, exportSchema = false)
